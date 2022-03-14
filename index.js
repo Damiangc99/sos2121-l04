629e8c19477f2630fe50aa7c376c5b57cbedd4be
Damiangc99 <99183982+Damiangc99@users.noreply.github.com>
2022-03-14 18:38:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ console.log(cool())
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/",(req,res)=>{
-    console.log("request /route");
+app.get("/cool",(req,res)=>{
+    console.log("request /faces route");
     res.send("<html><body><h1>"+cool()+"<h1></h1></body></html>");
+});
+
+app.get("/",(req,res)=>{
+    console.log("request /time route");
+    res.send("<html><body><h1>"+new Date()+"<h1></h1></body></html>");
 });
 
 
